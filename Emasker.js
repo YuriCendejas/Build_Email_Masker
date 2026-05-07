d987c7e4fb71a061137bbacf155b75be35b1471b
Yuri Cendejas <yuricendejas@outlook.com>
2026-05-07 15:45:00 -0400

    Update Emasker.js

--- Emasker.js
+++ Emasker.js
@@ -1,6 +1,7 @@
-function maskEmail("email"){
-
-}
 const email = "yuricendejas@outlook.com"; 
 /*3). Outside the function, you should have an email variable.
 4). You should assign a valid email address to your email variable. */
+
+function maskEmail(email){
+console.log(maskEmail(email));
+}
