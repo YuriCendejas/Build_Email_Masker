4c3801868ea04bf32fbaf37149619983b482b07b
Yuri Cendejas <yuricendejas@outlook.com>
2026-05-07 19:55:10 -0400

    Update Emasker.js

--- Emasker.js
+++ Emasker.js
@@ -5,5 +5,12 @@ const email = "yuricendejas@outlook.com";
 function maskEmail(email){
     const at = email.indexOf("@"); // to find the @ sign
 const username = email.slice(0,at); // it cuts from 0 " " to @ but not including it @ .
-}
+const first = username[0]; // first letter
+const last = username[username.length -1]; // last letter
+// find the length:
+
+// creating the stars: (astricks **)
+const masked = first + last + "*".repeat(username.length - 2 );  // why "2" ? bc it keeps the first and last letter , the middle will get masked
+} 
+return masked;
 console.log(maskEmail(email));
