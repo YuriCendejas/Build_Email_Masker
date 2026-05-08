db5e8a578502cbf9e5b98893e207be97a2d77ccd
Yuri Cendejas <yuricendejas@outlook.com>
2026-05-07 20:05:06 -0400

    updated

--- Emasker.js
+++ Emasker.js
@@ -4,13 +4,18 @@ const email = "yuricendejas@outlook.com";
 
 function maskEmail(email){
     const at = email.indexOf("@"); // to find the @ sign
-const username = email.slice(0,at); // it cuts from 0 " " to @ but not including it @ .
+const username = email.slice(0, at); // it cuts from 0 " " to @ but not including it @ .
+const domain = email.slice(at); // stops at the @ and only chops whats the username part.
 const first = username[0]; // first letter
 const last = username[username.length -1]; // last letter
 // find the length:
 
 // creating the stars: (astricks **)
-const masked = first + last + "*".repeat(username.length - 2 );  // why "2" ? bc it keeps the first and last letter , the middle will get masked
+const masked = first +"*".repeat(username.length - 2 ) + last;  // why "2" ? bc it keeps the first and last letter , the middle will get masked.
+return masked + domian; 
 } 
-return masked;
+
+/*the domain too bc thats the "@ " like we wanna hide
+ the username part but not the domain website part after the @ sign  .
+  like ex: "@outlook.com" we wanna keep */
 console.log(maskEmail(email));
